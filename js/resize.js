49530ffb820c463399f1c8a145b00f1efe198ed8
Dmitry Mamonov <dmitry.mamonov@dataart.com>
2018-11-30 23:16:13 +0300

    add resize images function

---
+++ js/resize.js
@@ -0,0 +1,30 @@
+var resize = (container, image) => {
+  var initImgRatio = image.width / image.height;
+
+  if (image.width > container.width && (image.width - container.width) > (image.height - container.height)) {
+    // img > container, width to fix
+    image.width = container.width;
+    image.height = image.width / initImgRatio;
+  } else if (image.width > container.width && (image.width - container.width) < (image.height - container.height)) {
+    // img > container, height to fix
+    image.height = container.height;
+    image.width = image.height * initImgRatio;
+  } else if (image.height > container.height) {
+    // img > container, height to fix
+    image.height = container.height;
+    image.width = image.height * initImgRatio;
+  } else if (image.height < container.height && (container.heigh - image.heigh) > (container.width - image.width)) {
+    // img < container, width to fix
+    image.width = container.width;
+    image.height = image.width / initImgRatio;
+  } else if (image.height < container.height && (container.heigh - image.heigh) < (container.width - image.width)) {
+    // img < container, height to fix
+    image.height = container.height;
+    image.width = image.height * initImgRatio;
+  }
+
+  var newDimensions = Object.assign({}, image);
+  return newDimensions;
+};
+
+export default resize;
