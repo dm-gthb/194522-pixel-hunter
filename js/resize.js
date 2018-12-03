b3a9910aef32032d07e2f2f55f20b798f3d73d42
Dmitry Mamonov <dmitry.mamonov@dataart.com>
2018-12-03 14:38:50 +0300

    resize small fix

--- js/resize.js
+++ js/resize.js
@@ -1,5 +1,7 @@
 const resize = (container, image) => {
   const initImgRatio = image.width / image.height;
+  const calculatedHeight = container.width / initImgRatio;
+  const calculatedWidth = container.height * initImgRatio;
   const imageIsWider = image.width > container.width;
   const imageVsContainerWidth = image.width - container.width;
   const imageVsContainerHeight = image.height - container.height;
@@ -8,16 +10,16 @@ const resize = (container, image) => {
 
   if (imageIsWider && (imageVsContainerWidth) > (imageVsContainerHeight)) {
     image.width = container.width;
-    image.height = image.width / initImgRatio;
+    image.height = calculatedHeight;
   } else if (imageIsWider && (imageVsContainerWidth) < (imageVsContainerHeight)) {
     image.height = container.height;
-    image.width = image.height * initImgRatio;
+    image.width = calculatedWidth;
   } else if (imageIsHigher) {
     image.height = container.height;
-    image.width = image.height * initImgRatio;
+    image.width = calculatedWidth;
   } else if (containerIsHigher) {
     image.width = container.width;
-    image.height = image.width / initImgRatio;
+    image.height = calculatedHeight;
   }
 
   const newDimensions = Object.assign({}, image);
